ca3f1d91fa8cbd2a900b38e1acfb5caee3a053d2
Giovanni Cemin <giovanni.cemin@studenti.unitn.it>
2022-11-21 14:22:41 +0100

    Created project dir

---
+++ project/create_data.py
@@ -0,0 +1,8 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import time
+
+import quimb as qu
+import quimb.tensor as qtn
+import quimb.linalg.base_linalg as la
+from itertools import product
