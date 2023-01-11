b2deab13f03dfdc1ce52dff321dbf71c1319ca63
Giovanni Cemin <giovanni.cemin@studenti.unitn.it>
2023-01-11 10:25:27 +0100

    New generation of data

--- project/create_data.py
+++ project/create_data.py
@@ -26,7 +26,7 @@ prms = {'L' : 20,                # length of spin chain
         'im_cutoff' : 1e-10,      # cutoff for TEBD algorithm, img t-e
         'tolerance' : 1e-3,      # Trotter tolerance for TEBD algorithm
         'verbose' : True,        # verbosity of the script
-        'num_traj' : 1,         # how many trajectories to do
+        'num_traj' : 30,         # how many trajectories to do
         # file to save the data
         'fname' : './data/data_training_S.hdf5'
         }
