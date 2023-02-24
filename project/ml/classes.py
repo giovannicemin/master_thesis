Fixed minus sign problem in the model, removed the use of complex numbers => now the model works! and the training is more stable

project/ml/classes.py
@@ -12,7 +12,7 @@ import h5py
 from torch.utils.data.dataset import Dataset
 
 from ml.utils import pauli_s_const, get_arch_from_layer_list, \
-    init_weights, Snake, FourierLayer
+    init_weights, Snake, FourierLayer, C_inf_Layer, Square
 
 class CustomDatasetFromHDF5(Dataset):
     '''Class implementing the Dataset object from HDF5 file.
@@ -675,13 +675,6 @@ class exp_LL_td_2(nn.Module):
         # where u is a unitary matrix and gamma a diagonal matrix of
         # time-dependent functions, of which the model learns the Fourier
         # decomposition.
-        frequencies = torch.Tensor([0.5, 1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5])
-        #frequencies = torch.Tensor([1.5])
-        self.gamma_net = nn.Sequential(FourierLayer(frequencies=frequencies,
-                                                    out_dim=self.data_dim,
-                                                    threshold=1e-2),
-                                       nn.ReLU())
-        self.gamma_normalization = 1#500
 
         # the model learn separately the real and complex part of the matrix at
         # the exponent
@@ -693,13 +686,43 @@ class exp_LL_td_2(nn.Module):
         nn.init.kaiming_uniform_(self.u_re, a=10)
         nn.init.kaiming_uniform_(self.u_im, a=10)
 
+        # NOTE: if the frequencies are learnable params it is important to initialize
+        # again the tensor, otherwise the model treats them equally (updated in the
+        # same manner)
+        #frequencies = torch.Tensor([0.5, 1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5])
+        frequencies = [1.5]
+        self.gamma_net = nn.Sequential(FourierLayer(frequencies=torch.Tensor(frequencies)),
+                                       #nn.Tanh(),
+                                       nn.Linear(2*len(frequencies), self.data_dim),
+                                       Square())
+        self.gamma_normalization = 1#100
+
+        self.omega_net = nn.Sequential(FourierLayer(frequencies=torch.Tensor(frequencies)),
+                                       #nn.Tanh(),
+                                       nn.Linear(2*len(frequencies), self.data_dim),
+                                       )
+        self.omega_normalization = 1#5
+
+        #frequencies = torch.Tensor([i for i in range(11)])
+        #frequencies = torch.Tensor([i for i in np.arange(0.5, 40, 0.5)])
+        #frequencies = torch.Tensor([0.1, 0.5, 1])
+        # self.gamma_net = nn.Sequential(FourierLayer(frequencies=frequencies,
+        #                                             out_dim=self.data_dim,
+        #                                             threshold=1e-2),
+        #                                nn.ReLU())
+        # self.gamma_net = nn.Sequential(C_inf_Layer(T=10, n=self.data_dim, m=1),
+        #                                nn.ReLU())
+        # self.gamma_normalization = 1#100
+
         # Hamiltonian parameters omega, also this is a vector of time-dependent
         # functions.
-        #frequencies = torch.Tensor([0.5, 1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5])
-        #frequencies = torch.Tensor([1.5])
-        self.omega_net = FourierLayer(frequencies=frequencies,
-                                      out_dim=self.data_dim, threshold=1e-2)
-        self.omega_normalization = 1#10
+        #frequencies = torch.Tensor([0.1, 0.2, 0.3, 0.5, 0.7, 1, 2])
+        #frequencies = torch.Tensor([i for i in range(5)])
+        #frequencies = torch.Tensor([0.1, 0.5, 1])
+        # self.omega_net = FourierLayer(frequencies=frequencies,
+        #                               out_dim=self.data_dim, threshold=1e-2)
+        # self.omega_net = C_inf_Layer(T=10, n=self.data_dim, m=1)
+        # self.omega_normalization = 1#5
 
     def forward(self, t, x):
         """Forward step of the model. """
@@ -726,15 +749,15 @@ class exp_LL_td_2(nn.Module):
         # Here I impose the fact c_re is symmetric and c_im antisymmetric
         re_1 = -4.*torch.einsum('mjk,nik,sij->smn', self.f, self.f, c_re )
         re_2 = -4.*torch.einsum('mik,njk,sij->smn', self.f, self.f, c_re )
-        im_1 = -4.*torch.einsum('mjk,nik,sij->smn', self.f, self.d, c_im )
-        im_2 =  4.*torch.einsum('mik,njk,sij->smn', self.f, self.d, c_im )
+        im_1 =  4.*torch.einsum('mjk,nik,sij->smn', self.f, self.d, c_im )
+        im_2 = -4.*torch.einsum('mik,njk,sij->smn', self.f, self.d, c_im )
         d_super_x_re = torch.add(re_1, re_2 )
         d_super_x_im = torch.add(im_1, im_2 )
         d_super_x = torch.add(d_super_x_re, d_super_x_im )
 
-        tr_id = -4.*torch.einsum('imj,sij->sm', self.f, c_im )
+        tr_id = 4.*torch.einsum('imj,sij->sm', self.f, c_im )
 
-        h_commutator_x =  4.* torch.einsum('ijk,sk->sji', self.f, omega).unsqueeze_(0)
+        h_commutator_x = -4.* torch.einsum('kij,sk->sij', self.f, omega).unsqueeze_(0)
 
         # building the Lindbladian operator
         L = torch.zeros(batch_size, self.data_dim+1, self.data_dim+1)
@@ -775,15 +798,15 @@ class exp_LL_td_2(nn.Module):
         # Here I impose the fact c_re is symmetric and c_im antisymmetric
         re_1 = -4.*torch.einsum('mjk,nik,ij->mn', self.f, self.f, c_re )
         re_2 = -4.*torch.einsum('mik,njk,ij->mn', self.f, self.f, c_re )
-        im_1 = -4.*torch.einsum('mjk,nik,ij->mn', self.f, self.d, c_im )
-        im_2 =  4.*torch.einsum('mik,njk,ij->mn', self.f, self.d, c_im )
+        im_1 =  4.*torch.einsum('mjk,nik,ij->mn', self.f, self.d, c_im )
+        im_2 = -4.*torch.einsum('mik,njk,ij->mn', self.f, self.d, c_im )
         d_super_x_re = torch.add(re_1, re_2 )
         d_super_x_im = torch.add(im_1, im_2 )
         d_super_x = torch.add(d_super_x_re, d_super_x_im )
 
-        tr_id = -4.*torch.einsum('imj,ij->m', self.f, c_im )
+        tr_id = 4.*torch.einsum('imj,ij->m', self.f, c_im )
 
-        h_commutator_x =  4.* torch.einsum('ijk,k->ji', self.f, omega)
+        h_commutator_x = -4.* torch.einsum('ijk,k->ij', self.f, omega)
 
         # building the Lindbladian operator
         L = torch.zeros(self.data_dim+1, self.data_dim+1)

project/ml/core.py
@@ -60,30 +60,24 @@ def train(model, criterion, optimizer, scheduler, train_loader, n_epochs, device
             summed_train_loss = np.append(summed_train_loss, loss.item())
 
             # regularization for U to be unitary
-            # u_re = model.MLP.u_re
-            # u_im = model.MLP.u_im
-            # matrix_1 = torch.mm(u_re, u_re.T) + torch.mm(u_im, u_im.T) - torch.eye(u_re.shape[0])
-            # matrix_1 = torch.abs(matrix_1)
-            # matrix_2 = torch.mm(u_im, u_re.T) - torch.mm(u_re, u_im.T)
-            # matrix_2 = torch.abs(matrix_2)
-            # loss += (1e-5)*( torch.sum(matrix_1) + torch.sum(matrix_2) )
-            # L2 regularization
-            # norm_weights = torch.norm(model.MLP.omega_net.weights)*\
-            #     torch.norm(model.MLP.gamma_net.weights)
-            # loss /= 1 + lamda*norm_weights
+            u_re = model.MLP.u_re
+            u_im = model.MLP.u_im
+            matrix_1 = torch.mm(u_re, u_re.T) + torch.mm(u_im, u_im.T) - torch.eye(u_re.shape[0])
+            matrix_1 = torch.abs(matrix_1)
+            matrix_2 = torch.mm(u_im, u_re.T) - torch.mm(u_re, u_im.T)
+            matrix_2 = torch.abs(matrix_2)
+            loss += (1e-2)*( torch.sum(matrix_1) + torch.sum(matrix_2) )
 
-            #--> regularization
-            l2_regularizer = torch.norm(model.MLP.omega_net.weight)*\
-                            torch.norm(model.MLP.gamma_net[0].weight)
-            l1_regularizer = torch.norm(model.MLP.omega_net.weight, 1) + \
-                            torch.norm(model.MLP.gamma_net[0].weight, 1)
-                            #torch.norm(model.MLP.u_re, 1) + \
-                            #torch.norm(model.MLP.u_im, 1)
+            # weights regularization
+            l2_regularizer = torch.norm(model.MLP.omega_net[1].weight) + \
+                            torch.norm(model.MLP.gamma_net[1].weight)
+            l1_regularizer = torch.norm(model.MLP.omega_net[1].weight, 1) + \
+                            torch.norm(model.MLP.gamma_net[1].weight, 1)
+                #torch.norm(model.MLP.omega_net.const, 1) + \
+                #torch.norm(model.MLP.gamma_net[0].const, 1)
             # Elastic Net
-            #loss += 0.01*( torch.norm(model.MLP.u_re) + torch.norm(model.MLP.u_im) )
             loss += alpha_1*l1_regularizer
-            loss /= 1 + alpha_2*l2_regularizer
-            #print(f"L1 {alpha_1*l1_regularizer}")
+            loss += alpha_2*l2_regularizer
 
             # backpropagate = calculate derivatives
             loss.backward(retain_graph=True)
@@ -115,7 +109,7 @@ def train(model, criterion, optimizer, scheduler, train_loader, n_epochs, device
 
         print('=== Mean train loss: {:.12f}'.format(summed_train_loss.mean()))
         print('=== lr: {:.5f}'.format(scheduler.get_last_lr()[0]))
-        #print(f'frequency gamma {model.MLP.gamma_net.frequencies}, omega {model.MLP.omega_net.frequencies}')
+        print(f'frequency gamma {model.MLP.gamma_net[0].frequencies.data}, omega {model.MLP.omega_net[0].frequencies.data}')
         mean_train_loss.append(summed_train_loss.mean())
 
     return mean_train_loss

project/ml/utils.py
@@ -9,163 +9,78 @@ import getopt
 import sys
 from torch.utils.data.sampler import SubsetRandomSampler
 
-# def custom_pruning_unstructured(module, name, threshold):
-#     """Prunes tensor corresponding to parameter called `name` in `module`
-#     by setting to zero if < threshold
-#     Modifies module in place (and also return the modified module)
-#     by:
-#     1) adding a named buffer called `name+'_mask'` corresponding to the
-#     binary mask applied to the parameter `name` by the pruning method.
-#     The parameter `name` is replaced by its pruned version, while the
-#     original (unpruned) parameter is stored in a new parameter named
-#     `name+'_orig'`.
-
-#     Args:
-#         module (nn.Module): module containing the tensor to prune
-#         name (string): parameter name within `module` on which pruning
-#                 will act.
-
-#     Returns:
-#         module (nn.Module): modified (i.e. pruned) version of the input
-#             module
-
-#     Examples:
-#         >>> m = nn.Linear(3, 4)
-#         >>> custum_pruning_unstructured(m, name='bias')
-#     """
-#     pruning = ThresholdPruning()
-#     print('ciao')
-#     pruning.apply(module, name)
-
-#     return module
-
-# class ThresholdPruning(prune.BasePruningMethod):
-#     PRUNING_TYPE = "unstructured"
-
-#     def __init__(self):
-#         self.threshold = 1e-2
-
-#     def compute_mask(self, tensor, default_mask):
-#         mask = torch.abs(tensor[:, :]) < self.threshold
-#         default_mask[mask] = 0
-#         return default_mask
-#         #return torch.abs(tensor) > self.threshold
-
-class SinusoidalLayer(nn.Module):
-    """ Custom Layer implementing sinusoids linear combination.
-
-
-    """
-    def __init__(self, T, m) -> None:
-        super().__init__()
-        self.m = m
-
-        self.frequency = 2*torch.pi/T
-
-        amplitude = torch.ones(m)
-        self.amplitude = nn.Parameter(amplitude)
-
-        phase = torch.zeros(m)
-        self.phase = nn.Parameter(phase)
-
-        const = torch.zeros(m)
-        self.const = nn.Parameter(const)
-
-        # init
-        nn.init.uniform_(self.amplitude, 0.1)
-
-    def forward(self, t):
-        batch_size = t.shape
-
-        # i -> index for the output vector
-        # b -> batch index
-
-        # putting everything in the right dimension
-        t_f_product = (t*self.frequency).unsqueeze(1)
-        t_f_product = t_f_product.repeat(self.m, 1)
-        phase = self.phase.unsqueeze(0).repeat(batch_size, 0)
-
-        p_function = torch.cos(t_f_product + phase)
-
-        product = torch.einsum("bi,i -> bi", p_function, self.amplitude)
-
-        const = self.const.unsqueeze(0).repeat(batch_size, 0)
-
-        return product + const
-
-
-class C_inf_Layer(nn.Module):
-    """ Custom layer implementing C^inf periodic functions
-
-    """
-    def __init__(self, T, n, m) -> None:
-        super().__init__()
-
-        self.layer = nn.Sequential(SinusoidalLayer(T, m),
-                                   nn.Tanh(),
-                                   nn.Linear(m, n),
-                                   nn.Tanh())
-    def forward(self, t):
-        return self.layer.forward(t)
-
-
 class FourierLayer(nn.Module):
-    """ Custom Layer to construct time function starting from
-    the Fourier transform.
+    """ Custom Layer that outputs the Fourier components.
 
-    For each output the model learns the 0 frequency (const) term
-    and the real anc complex part for each frequency in input.
-    Note the frequencies are learnable parameters!
+    Given the number of frequency the model outputs
+    A*cos(t*f + phi) + c   first half
+    A*sin(t*f + phi) + c   second half
 
-    Because of the use of periodic functions the learning procedure of this
-    model is really delicate, and the risk of ending in a local minima very high.
+    This should be Fourier decomposition of the functions.
 
     Parameters
     ----------
     frequencies : array float
         Init values for the frequencies
-    out_dim : int
-        Dimention of the output
-    threshold : float
-        Threshold for pruning
-    impose_positivity : bool
-        Whether or not to have a >=0 output.
+    require_amplitude : bool
+        Whether to set amplitude != 1. Becomes usefull
+        in case one is applying the non-linear function.
+    require_constant : bool
+        Whether to have constant != 0. Becomes usefull
+        in case one is ammplying the non-linear function.
+    require_phase : bool
+        Whether to put the phase, bounded between -pi/2, pi/2
     """
-    def __init__(self, frequencies, out_dim:int, threshold:float,
-                 impose_positivity:bool = False):
+    def __init__(self, frequencies, require_amplitude=False,
+                 require_constant=False, require_phase=False):
         super().__init__()
         N_freq = len(frequencies)
 
-        # create different set of frequencies for each output
-        frequencies = frequencies.repeat(out_dim, 1)
-        self.frequencies = nn.Parameter(frequencies, requires_grad=False)
+        self.frequencies = nn.Parameter(frequencies, requires_grad=True)
 
-        weight = torch.ones((out_dim, 2*(N_freq)+1))
-        self.weight = nn.Parameter(weight)
+        amplitude = torch.ones(2*N_freq)
+        if require_amplitude:
+            self.amplitude = nn.Parameter(amplitude)
+            nn.init.normal_(self.amplitude, 0, 0.1)
+        else:
+            self.amplitude = amplitude
 
-        self.out_dim = out_dim
-        self.threshold = threshold
-        self.pos = impose_positivity
+        const = torch.zeros_like(amplitude)
+        if require_constant:
+            self.const = nn.Parameter(const)
+            nn.init.normal_(self.const, 0, 0.1)
+        else:
+            self.const = const
 
-        # initialize weights
-        nn.init.uniform_(self.weight, 0)
+        phase = torch.zeros(N_freq)
+        if require_phase:
+            self.phase = nn.Parameter(phase)
+            nn.init.normal_(self.phase, 0, 0.1)
+        else:
+            self.phase = phase
 
     def forward(self, t):
+        batch_size = t.shape[0]
         # bound the frequencies
-        frequencies = self.frequencies.clamp(1e-2, 20)
+        #frequencies = self.frequencies.clamp(1e-2, 20)
+        # bound the phases
+        phase = self.phase.clamp(-0.5*torch.pi, 0.5*torch.pi)
 
-        t_f_product = torch.einsum('b,wf -> bwf', t, frequencies)
-        F = torch.cat((torch.cos(t_f_product),\
-                       torch.sin(t_f_product)), dim=-1)
-        # add the 0 frequency term = constant
-        F = torch.cat((F, torch.ones(len(t), self.out_dim).unsqueeze_(-1)), dim=-1)
+        t_f_product = torch.einsum('b,f -> bf', t, self.frequencies)
+        argument = t_f_product + phase.repeat(batch_size, 1)
+        F = torch.cat((torch.cos(argument),\
+                       torch.sin(argument)), dim=-1)
 
-        # w -> index for the output vector
-        # b -> batch index
-        # n -> Fourier decomposition element
-        w = torch.einsum('wn,bwn->bw', self.weight, F)
+        return torch.einsum('bf, f -> bf', F, self.amplitude) + \
+            self.const.repeat(batch_size, 1)
 
-        return w
+
+class Square(nn.Module):
+    def __init__(self) -> None:
+        super().__init__()
+
+    def forward(self, x):
+        return torch.square(x)
 
 
 class Snake(nn.Module):
@@ -259,11 +174,13 @@ def pauli_s_const():
     abc = oe.contract('aij,bjk,cki->abc', base_F, base_F, base_F )
     acb = oe.contract('aij,bki,cjk->abc', base_F, base_F, base_F )
 
-    f = np.real( 1j*0.5*(abc - acb) )
-    d = np.real( 0.5*(abc + acb) )
+    # added -, and put 0.25 instead of 0.5
+    f = np.real( -1j*0.25*(abc - acb) )
+    d = np.real( 0.25*(abc + acb) )
 
     # return as a torch tensor
     return torch.from_numpy(f).float(), torch.from_numpy(d).float()
+
 
 def ensure_empty_dir(directory):
     if len(os.listdir(directory)) != 0:
@@ -368,3 +285,186 @@ def get_params_from_cmdline(argv, default_params=None):
             default_params['working_dir'] = arg
 
     return default_params
+
+class SinusoidalLayer(nn.Module):
+    """ Custom Layer implementing sinusoids linear combination.
+
+    This layer take tie as input and output the
+    A * cos(t*w + ph) + c
+        where w  = frequency
+              ph = phase
+              A, c = amplitude and constant
+    Arguments
+    ---------
+    T : float
+        Maximum time
+    m : int
+        Number of independent periodic functions.
+    """
+    def __init__(self, T, m) -> None:
+        super().__init__()
+        self.m = m
+
+        self.frequency = nn.Parameter(torch.Tensor([1.5]))
+
+        amplitude = torch.ones(m)
+        self.amplitude = nn.Parameter(amplitude)
+
+        phase = torch.zeros(m)
+        self.phase = nn.Parameter(phase)
+
+        const = torch.zeros(m)
+        self.const = nn.Parameter(const)
+
+        # init
+        nn.init.uniform_(self.amplitude, 0.1)
+
+    def forward(self, t):
+        batch_size = t.shape[0]
+
+        # i -> index for the output vector
+        # b -> batch index
+
+        # putting everything in the right dimension
+        t_f_product = (t*self.frequency).unsqueeze(1)
+        t_f_product = t_f_product.repeat(1, self.m)
+        phase = self.phase.repeat(batch_size, 1)
+
+        p_function = torch.cos(t_f_product + phase)
+
+        product = torch.einsum("bi,i -> bi", p_function, self.amplitude)
+
+        const = self.const.repeat(batch_size, 1)
+
+        return product + const
+
+
+class C_inf_Layer(nn.Module):
+    """ Custom layer implementing C^inf periodic functions
+
+    Arguments
+    ---------
+    T : float
+        Maximum time, the minimim frequency is caluclated accordingly
+    m : int
+        Number of indepdendent periodic functions
+    n : int
+        Number of nodes = output dimension
+    """
+    def __init__(self, T, n, m) -> None:
+        super().__init__()
+
+        self.layer = nn.Sequential(SinusoidalLayer(T, m),
+                                   nn.Tanh(),
+                                   nn.Linear(m, n),
+                                   nn.Tanh())
+    def forward(self, t):
+        return self.layer.forward(t)
+
+
+# def custom_pruning_unstructured(module, name, threshold):
+#     """Prunes tensor corresponding to parameter called `name` in `module`
+#     by setting to zero if < threshold
+#     Modifies module in place (and also return the modified module)
+#     by:
+#     1) adding a named buffer called `name+'_mask'` corresponding to the
+#     binary mask applied to the parameter `name` by the pruning method.
+#     The parameter `name` is replaced by its pruned version, while the
+#     original (unpruned) parameter is stored in a new parameter named
+#     `name+'_orig'`.
+
+#     Args:
+#         module (nn.Module): module containing the tensor to prune
+#         name (string): parameter name within `module` on which pruning
+#                 will act.
+
+#     Returns:
+#         module (nn.Module): modified (i.e. pruned) version of the input
+#             module
+
+#     Examples:
+#         >>> m = nn.Linear(3, 4)
+#         >>> custum_pruning_unstructured(m, name='bias')
+#     """
+#     pruning = ThresholdPruning()
+#     print('ciao')
+#     pruning.apply(module, name)
+
+#     return module
+
+# class ThresholdPruning(prune.BasePruningMethod):
+#     PRUNING_TYPE = "unstructured"
+
+#     def __init__(self):
+#         self.threshold = 1e-2
+
+#     def compute_mask(self, tensor, default_mask):
+#         mask = torch.abs(tensor[:, :]) < self.threshold
+#         default_mask[mask] = 0
+#         return default_mask
+#         #return torch.abs(tensor) > self.threshold
+
+class FourierLayer_backup(nn.Module):
+    """ Custom Layer that outputs the Fourier components.
+
+    For each output the model learns the 0 frequency (const) term
+    and the real anc complex part for each frequency in input.
+    Note the frequencies are learnable parameters!
+
+    Because of the use of periodic functions the learning procedure of this
+    model is really delicate, and the risk of ending in a local minima very high.
+
+    Parameters
+    ----------
+    frequencies : array float
+        Init values for the frequencies
+    out_dim : int
+        Dimention of the output
+    threshold : float
+        Threshold for pruning
+    impose_positivity : bool
+        Whether or not to have a >=0 output.
+    """
+    def __init__(self, frequencies, constant=False, phase=False):
+        super().__init__()
+        N_freq = len(frequencies)
+
+        # create different set of frequencies for each output
+        frequencies = frequencies.repeat(out_dim, 1)
+        self.frequencies = nn.Parameter(frequencies, requires_grad=True)
+
+        weight = torch.ones((out_dim, 2*N_freq))
+        self.weight = nn.Parameter(weight)
+        const = torch.ones_like(weight)
+        self.const = nn.Parameter(const)
+
+        self.out_dim = out_dim
+        self.threshold = threshold
+
+        # initialize weights
+        nn.init.normal_(self.weight, 0, 0.1)
+        nn.init.normal_(self.const, 0, 0.1)
+
+    def forward(self, t):
+        batch_size = t.shape[0]
+        # bound the frequencies
+        #frequencies = self.frequencies.clamp(1e-2, 20)
+
+        t_f_product = torch.einsum('b,wf -> bwf', t, self.frequencies)
+        F = torch.cat((torch.cos(t_f_product),\
+                       torch.sin(t_f_product)), dim=-1)
+        # add the linear term
+        #linear_term = torch.einsum('w,b->bw', self.linear, t)
+        #F = torch.cat((F, linear_term.unsqueeze_(-1)), dim=-1)
+
+        # w -> index for the output vector
+        # b -> batch index
+        # n -> Fourier decomposition element
+        # w = torch.einsum('wn,bwn->bw', self.weight, F)
+        w = torch.einsum('wn,bwn->bwn', self.weight, F)
+
+        # I want to constrain the constant term to be positive
+        const = self.const.repeat(batch_size, 1, 1)
+
+        # sum ove the Fourier components
+        return torch.einsum('bwn->bw', w+const)

project/models.py
@@ -401,21 +401,21 @@ class Lindbladian(ABC):
 
     def forward(self, t, x):
 
-        re_c = self.kossakowski(t).real
-        im_c = self.kossakowski(t).imag
+        re_c = self.kossakowski(t).real.float()
+        im_c = self.kossakowski(t).imag.float()
 
         # Here I impose the fact c_re is symmetric and c_im antisymmetric
         re_1 = -4.*torch.einsum('mjk,nik,ij->mn', self.f, self.f, re_c )
         re_2 = -4.*torch.einsum('mik,njk,ij->mn', self.f, self.f, re_c )
-        im_1 = -4.*torch.einsum('mjk,nik,ij->mn', self.f, self.d, im_c )
-        im_2 =  4.*torch.einsum('mik,njk,ij->mn', self.f, self.d, im_c )
+        im_1 =  4.*torch.einsum('mjk,nik,ij->mn', self.f, self.d, im_c )
+        im_2 = -4.*torch.einsum('mik,njk,ij->mn', self.f, self.d, im_c )
         d_super_x_re = torch.add(re_1, re_2 )
         d_super_x_im = torch.add(im_1, im_2 )
         d_super_x = torch.add(d_super_x_re, d_super_x_im )
 
-        tr_id = -4.*torch.einsum('imj,ij->m', self.f, im_c )
+        tr_id = 4.*torch.einsum('imj,ij->m', self.f, im_c )
 
-        h_commutator_x =  4.* torch.einsum('ijk,k->ji', self.f, self.omega(t))
+        h_commutator_x = -4.* torch.einsum('ijk,k->ij', self.f, self.omega(t))
 
         # building the Lindbladian operator
         L = torch.zeros(16, 16)